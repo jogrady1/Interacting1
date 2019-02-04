6019d526874540922a85887fea341ddb2fa02f68
jogrady1 <46456976+jogrady1@users.noreply.github.com>
2019-02-04 12:43:10 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,23 @@
+// Interacting
+
+//img 
+
+
+
+function myFunction(imgs) {
+  // Get the expanded image
+  var expandImg = document.getElementById("expandedImg");
+  // Get the image text
+  var imgText = document.getElementById("imgtext");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element
+  expandImg.parentElement.style.display = "block";
+}
+
+
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+}
